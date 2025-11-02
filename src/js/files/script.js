918dc8d4f625e55e3c3c8a8814ace369e59c2f00
d3n2dboy <d3n2dboy@gmail.com>
2025-11-03 00:12:40 +0500

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,5 @@
+// Подключение функционала "Чертогов Фрилансера"
+//import { log } from "gulp-util";
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
